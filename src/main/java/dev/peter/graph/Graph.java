5d712faadda74e13d4537aee5fd43117d6b19930
gbd850 <mmyszaki@gmail.com>
2024-03-31 22:38:06 +0200

    Add Dijkstra Shortest Path

--- src/main/java/dev/peter/graph/Graph.java
+++ src/main/java/dev/peter/graph/Graph.java
@@ -95,4 +95,80 @@ public class Graph {
         return path.stream().mapToInt(Integer::intValue).toArray();
     }
 
+    public static int[] dijkstra_AdjList(GraphEdge[][] graph, int source, int value) {
+        final boolean[] seen = new boolean[graph.length];
+        final int[] dists = new int[graph.length];
+        final int[] prev = new int[graph.length];
+        Arrays.fill(dists, -1);
+        Arrays.fill(prev, -1);
+
+        dists[source] = 0;
+
+        while (hasUnvisited(seen, dists)) {
+            final int curr = getLowestUnvisited(seen, dists);
+            seen[curr] = true;
+
+            final GraphEdge[] adjs = graph[curr];
+            for (int i = 0; i < adjs.length; i++) {
+
+                final GraphEdge edge = adjs[i];
+
+                if (seen[edge.getTo()]) {
+                    continue;
+                }
+
+                final int dist = dists[curr] + edge.getWeight();
+
+                if (dist < dists[edge.getTo()] || dists[edge.getTo()] < 0) {
+                    dists[edge.getTo()] = dist;
+                    prev[edge.getTo()] = curr;
+                }
+            }
+        }
+
+        final List<Integer> out = new ArrayList<>();
+        int curr = value;
+
+        while (prev[curr] != -1) {
+            out.add(curr);
+            curr = prev[curr];
+        }
+
+        out.add(source);
+        Collections.reverse(out);
+
+        return out.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static int getLowestUnvisited(boolean[] seen, int[] dists) {
+        int index = -1;
+        int lowestDistance = -1;
+
+        for (int i = 0; i < seen.length; i++) {
+            if (seen[i]) {
+                continue;
+            }
+
+            if ((lowestDistance < 0 || lowestDistance > dists[i]) && dists[i] > -1) {
+                lowestDistance = dists[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    private static boolean hasUnvisited(boolean[] seen, int[] dists) {
+        boolean has = false;
+
+        for (int i = 0; i < seen.length; i++) {
+            if (!seen[i] && dists[i] > -1) {
+                has = true;
+                break;
+            }
+        }
+
+        return has;
+    }
+
 }
--- src/test/java/dev/peter/graph/GraphTest.java
+++ src/test/java/dev/peter/graph/GraphTest.java
@@ -2,6 +2,9 @@ package dev.peter.graph;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,7 +20,7 @@ class GraphTest {
             {0, 0, 0, 1, 0, 0, 1},
     };
 
-    private final GraphEdge[][] graphList = new GraphEdge[][]{
+    private final GraphEdge[][] graphList1 = new GraphEdge[][]{
             {
                     new GraphEdge(1, 3),
                     new GraphEdge(2, 1)
@@ -45,6 +48,42 @@ class GraphTest {
             }
     };
 
+    private final GraphEdge[][] graphList2 = new GraphEdge[][]{
+            {
+                    new GraphEdge(1, 3),
+                    new GraphEdge(2, 1)
+            },
+            {
+                    new GraphEdge(0, 3),
+                    new GraphEdge(2, 4),
+                    new GraphEdge(4, 1)
+            },
+            {
+                    new GraphEdge(1, 4),
+                    new GraphEdge(3, 7),
+                    new GraphEdge(0, 1)
+            },
+            {
+                    new GraphEdge(2, 7),
+                    new GraphEdge(4, 5),
+                    new GraphEdge(6, 1)
+            },
+            {
+                    new GraphEdge(1, 1),
+                    new GraphEdge(3, 5),
+                    new GraphEdge(5, 2)
+            },
+            {
+                    new GraphEdge(6, 1),
+                    new GraphEdge(4, 2),
+                    new GraphEdge(2, 18)
+            },
+            {
+                    new GraphEdge(3, 1),
+                    new GraphEdge(5, 1)
+            }
+    };
+
     @Test
     void givenAdjMatrixGraph_shouldBreadthFirstSearchReturnPath() {
         int[] expected = new int[]{0, 1, 4, 5, 6};
@@ -63,13 +102,20 @@ class GraphTest {
     void givenAdjListGraph_shouldDepthFirstSearchReturnPath() {
         int[] expected = new int[]{0, 1, 4, 5, 6};
 
-        assertArrayEquals(expected, Graph.depthFirstSearch_AdjList(graphList, 0, 6));
+        assertArrayEquals(expected, Graph.depthFirstSearch_AdjList(graphList1, 0, 6));
     }
 
     @Test
     void givenAdjListGraph_shouldDepthFirstSearchReturnEmptyArray() {
         int[] expected = new int[0];
 
-        assertArrayEquals(expected, Graph.depthFirstSearch_AdjList(graphList, 6, 0));
+        assertArrayEquals(expected, Graph.depthFirstSearch_AdjList(graphList1, 6, 0));
+    }
+
+    @Test
+    void givenAdjListGraph_shouldDijkstraReturnPath() {
+        int[] expected = new int[]{0, 1, 4, 5, 6};
+
+        assertArrayEquals(expected, Graph.dijkstra_AdjList(graphList2, 0, 6));
     }
 }
